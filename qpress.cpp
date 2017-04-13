bd97be223f07d8f7e26e428371374493dbf19000
Pierre Lavaux <pierre@lavaux.lv>
2017-04-13 21:28:14 +0200

    cppcheck fix
    Passing the result of c_str() to a function that takes std::string as argument no. 1 is slow and redundant.

--- qpress.cpp
+++ qpress.cpp
@@ -923,7 +923,7 @@ void compress_directory(string base_dir, string pattern)
 		{
 			path = api_path + string(entry->d_name);
 
-			if(is_dir(path.c_str()) && string(entry->d_name) != "." && string(entry->d_name) != "..")
+			if(is_dir(path) && string(entry->d_name) != "." && string(entry->d_name) != "..")
 			{
 				godown(entry->d_name);
 				compress_directory(path, pattern);
